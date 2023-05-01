d5f58af1bff021fa167c538925ac189d034bcff9
charmley <trevor.charmley@gmail.com>
2023-04-30 17:39:16 -0700

    added check for templates to bachrc

--- setup/install_all.sh
+++ setup/install_all.sh
@@ -9,11 +9,11 @@ fi
 echo "Starting TGW Setup."
 echo "Step 1: update and upgrade the system"
 
-# sudo apt-get update
-# if [ $? != 0 ]
-# then 
-#     echo "ERROR: Updating packages"; exit 1
-# fi
+sudo apt-get update
+if [ $? != 0 ]
+then 
+    echo "ERROR: Updating packages"; exit 1
+fi
 
 # Required packages 
     # python3-pip
@@ -25,89 +25,95 @@ echo "Step 1: update and upgrade the system"
     # paho-mqtt
     # sysstat
 
-# echo "Step 2: Install required pachages"
+echo "Step 2: Install required pachages"
 
-# sudo apt-get install python3-pip
-# if [ $? != 0 ]
-# then 
-#     echo "ERROR: Installing PIP"; exit 1
-# fi
+sudo apt-get install python3-pip
+if [ $? != 0 ]
+then 
+    echo "ERROR: Installing PIP"; exit 1
+fi
 
-# sudo apt-get install python3-smbus
-# if [ $? != 0 ]
-# then 
-#     echo "ERROR: Installing SMBUS"; exit 1
-# fi
+sudo apt-get install python3-smbus
+if [ $? != 0 ]
+then 
+    echo "ERROR: Installing SMBUS"; exit 1
+fi
 
-# sudo apt-get install -y mosquitto
-# if [ $? != 0 ]
-# then 
-#     echo "ERROR: Installing Mosquitto"; exit 1
-# fi
+sudo apt-get install -y mosquitto
+if [ $? != 0 ]
+then 
+    echo "ERROR: Installing Mosquitto"; exit 1
+fi
 
-# sudo apt-get install -y mosquitto-clients
-# if [ $? != 0 ]
-# then 
-#     echo "ERROR: Installing Mosquitto Client"; exit 1
-# fi
+sudo apt-get install -y mosquitto-clients
+if [ $? != 0 ]
+then 
+    echo "ERROR: Installing Mosquitto Client"; exit 1
+fi
 
-# echo "Step 3: Install Supervisor"
-# sudo apt-get install -y supervisor
-# echo "SUPERVISOR $?"
-# if [ $? != 0 ]
-# then 
-#     echo "ERROR: Installing Supervisor"; exit 1
-# fi
+echo "Step 3: Install Supervisor"
+sudo apt-get install -y supervisor
+echo "SUPERVISOR $?"
+if [ $? != 0 ]
+then 
+    echo "ERROR: Installing Supervisor"; exit 1
+fi
 
-# sudo pip3 install ndeflib
-# if [ $? != 0 ]
-# then 
-#     echo "ERROR: Installing NDEFLIB"; exit 1
-# fi
+sudo pip3 install ndeflib
+if [ $? != 0 ]
+then 
+    echo "ERROR: Installing NDEFLIB"; exit 1
+fi
 
-# sudo pip3 install paho-mqtt
-# if [ $? != 0 ]
-# then 
-#     echo "ERROR: Installing paho-mqtt"; exit 1
-# fi
+sudo pip3 install paho-mqtt
+if [ $? != 0 ]
+then 
+    echo "ERROR: Installing paho-mqtt"; exit 1
+fi
 
-# sudo apt-get install -y sysstat
-# if [ $? != 0 ]
-# then 
-#     echo "ERROR: Installing sysstat"; exit 1
-# fi
+sudo apt-get install -y sysstat
+if [ $? != 0 ]
+then 
+    echo "ERROR: Installing sysstat"; exit 1
+fi
 
-# sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
-# if [ $? != 0 ]
-# then 
-#     echo "ERROR: Installing adafruit-circuitpython-neopixel"; exit 1
-# fi
+sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
+if [ $? != 0 ]
+then 
+    echo "ERROR: Installing adafruit-circuitpython-neopixel"; exit 1
+fi
 
-# sudo python3 -m pip install --force-reinstall adafruit-blinka
-# if [ $? != 0 ]
-# then 
-#     echo "ERROR: Installing adafruit-blinka"; exit 1
-# fi
+sudo python3 -m pip install --force-reinstall adafruit-blinka
+if [ $? != 0 ]
+then 
+    echo "ERROR: Installing adafruit-blinka"; exit 1
+fi
 
 
 
 # Install Seed studio 
 # Seeed studio is added as a git submodule of this repo
-# git submodule update --init
-# echo "Step 4: Install Seeed studio voice card"
-# cd ../seeed-voicecard/ #change directory to seeed voice card. 
-# echo $PWD
-# sudo ./install.sh
-# if [ $? != 0 ]
-# then 
-#     echo "ERROR: Installing SeedStudio voice card"; exit 1
-# fi
-# cd - #change directory back
+git submodule update --init
+echo "Step 4: Install Seeed studio voice card"
+cd ../seeed-voicecard/ #change directory to seeed voice card. 
+echo $PWD
+sudo ./install.sh
+if [ $? != 0 ]
+then 
+    echo "ERROR: Installing SeedStudio voice card"; exit 1
+fi
+cd - #change directory back
 
 # Add temp files to bashrc
-# echo "export PATH="~/thegoodwand/templates:$PATH"" > temp
-# cat temp >> ~/.bashrc
-# rm temp
+
+if grep -Fq "export PATH=~/thegoodwand/templates:" ~/.bashrc
+then 
+    echo "Templates already added to ./bashrc"
+else
+    echo "export PATH="~/thegoodwand/templates:$PATH"" > temp
+    cat temp >> ~/.bashrc
+    rm temp
+fi
 
 #Pass path to the all the services to daemon script.
 echo "Step 3: Setting up system daemons script"
