d2142facbffbf359f1eaa71f6b10644d60a5b194
charmley <trevor.charmley@gmail.com>
2023-05-20 14:25:23 -0700

    updated to add VLC and change IR pins

--- setup/install_all.sh
+++ setup/install_all.sh
@@ -2,7 +2,7 @@
 
 if [ $USER = "root" ]
 then 
-    echo "ERROR: Do not run as SUDO"
+    echo "[ERROR: install_all.sh $LINENO] Do not run as SUDO"
     exit 1
 fi
 
@@ -12,7 +12,7 @@ echo "Step 1: update and upgrade the system"
 sudo apt-get update
 if [ $? != 0 ]
 then 
-    echo "ERROR: Updating packages"; exit 1
+    echo "[ERROR: install_all.sh $LINENO] Updating packages"; exit 1
 fi
 
 # Required packages 
@@ -30,25 +30,37 @@ echo "Step 2: Install required pachages"
 sudo apt-get install python3-pip
 if [ $? != 0 ]
 then 
-    echo "ERROR: Installing PIP"; exit 1
+    echo "[ERROR: install_all.sh $LINENO] Installing PIP"; exit 1
 fi
 
 sudo apt-get install python3-smbus
 if [ $? != 0 ]
 then 
-    echo "ERROR: Installing SMBUS"; exit 1
+    echo "[ERROR: install_all.sh $LINENO] Installing SMBUS"; exit 1
 fi
 
 sudo apt-get install -y mosquitto
 if [ $? != 0 ]
 then 
-    echo "ERROR: Installing Mosquitto"; exit 1
+    echo "[ERROR: install_all.sh $LINENO] Installing Mosquitto"; exit 1
 fi
 
 sudo apt-get install -y mosquitto-clients
 if [ $? != 0 ]
 then 
-    echo "ERROR: Installing Mosquitto Client"; exit 1
+    echo "[ERROR: install_all.sh $LINENO] Installing Mosquitto Client"; exit 1
+fi
+
+sudo apt install -y vlc
+if [ $? != 0 ]
+then 
+    echo "[ERROR: install_all.sh $LINENO] Installing VLC"; exit 1
+fi
+
+sudo apt-get install alsa-base pulseaudio
+if [ $? != 0 ]
+then 
+    echo "[ERROR: install_all.sh $LINENO] Installing alsa-base pulseaudio Client"; exit 1
 fi
 
 echo "Step 3: Install Supervisor"
@@ -56,39 +68,44 @@ sudo apt-get install -y supervisor
 echo "SUPERVISOR $?"
 if [ $? != 0 ]
 then 
-    echo "ERROR: Installing Supervisor"; exit 1
+    echo "[ERROR: install_all.sh $LINENO] Installing Supervisor"; exit 1
 fi
 
 sudo pip3 install ndeflib
 if [ $? != 0 ]
 then 
-    echo "ERROR: Installing NDEFLIB"; exit 1
+    echo "[ERROR: install_all.sh $LINENO] Installing NDEFLIB"; exit 1
 fi
 
 sudo pip3 install paho-mqtt
 if [ $? != 0 ]
 then 
-    echo "ERROR: Installing paho-mqtt"; exit 1
+    echo "[ERROR: install_all.sh $LINENO] Installing paho-mqtt"; exit 1
 fi
 
 sudo apt-get install -y sysstat
 if [ $? != 0 ]
 then 
-    echo "ERROR: Installing sysstat"; exit 1
+    echo "[ERROR: install_all.sh $LINENO] Installing sysstat"; exit 1
 fi
 
 sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
 if [ $? != 0 ]
 then 
-    echo "ERROR: Installing adafruit-circuitpython-neopixel"; exit 1
+    echo "[ERROR: install_all.sh $LINENO] Installing adafruit-circuitpython-neopixel"; exit 1
 fi
 
 sudo python3 -m pip install --force-reinstall adafruit-blinka
 if [ $? != 0 ]
 then 
-    echo "ERROR: Installing adafruit-blinka"; exit 1
+    echo "[ERROR: install_all.sh $LINENO] Installing adafruit-blinka"; exit 1
 fi
 
+sudo pip3 install python-vlc
+if [ $? != 0 ]
+then 
+    echo "[ERROR: install_all.sh $LINENO] Installing python-vlc"; exit 1
+fi
 
 
 # Install Seed studio 
@@ -99,7 +116,7 @@ cd ../seeed-voicecard/ #change directory to seeed voice card.
 sudo ./install.sh
 if [ $? != 0 ]
 then 
-    echo "ERROR: Installing SeedStudio voice card"; exit 1
+    echo "[ERROR: install_all.sh $LINENO] Installing SeedStudio voice card"; exit 1
 fi
 cd - #change directory back
 
@@ -115,12 +132,23 @@ else
     rm temp
 fi
 
+if grep -Fq "enable_uart=1" /boot/config.txt
+then
+        echo "Uart Already enabled"
+else
+    echo "enable_uart=1" | sudo tee -a /boot/config.txt
+fi
+
+echo "Updating IR Pins"
+sudo sed -i 's/#dtoverlay=gpio-ir,gpio_pin=17/dtoverlay=gpio-ir,gpio_pin=23/g' /boot/config.txt
+sudo sed -i 's/#dtoverlay=gpio-ir-tx,gpio_pin=18/dtoverlay=gpio-ir,gpio_pin=22/g' /boot/config.txt
+
 #Pass path to the all the services to daemon script.
 echo "Step 3: Setting up system daemons script"
 sudo ./daemons.sh "$USER/thegoodwand/services" $USER
 if [ $? != 0 ]
 then 
-    echo "ERROR: Running Deamon script"; exit 1
+    echo "[ERROR: install_all.sh $LINENO] Running Deamon script"; exit 1
 fi
 
 sudo reboot now
