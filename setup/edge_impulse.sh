36a3122086509fcb9affc2ee04253ac02e641856
Jim Horwitz <jhorwitz1011@gmail.com>
2023-06-23 00:09:57 -0400

    colos/lumos/extivious/mousike support

--- setup/edge_impulse.sh
+++ setup/edge_impulse.sh
@@ -1,5 +1,7 @@
-sudo apt update
-sudo apt upgrade
 curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
 sudo apt install -y gcc g++ make build-essential nodejs sox gstreamer1.0-tools gstreamer1.0-plugins-good gstreamer1.0-plugins-base gstreamer1.0-plugins-base-apps
 npm config set user root && sudo npm install edge-impulse-linux -g --unsafe-perm
+
+sudo apt-get install libatlas-base-dev libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev libopenjp2-7 libgtk-3-0 libswscale-dev libavformat58 libavcodec58
+pip3 install pyaudio
+pip3 install edge_impulse_linux -i https://pypi.python.org/simple
