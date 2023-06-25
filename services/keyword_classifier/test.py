3725e6cc2a0dfcdab8980afa39e8d0110e226701
Jim Horwitz <jhorwitz1011@gmail.com>
2023-06-25 10:36:33 -0400

    test.py added

---
+++ services/keyword_classifier/test.py
@@ -0,0 +1,18 @@
+sys.path.append(os.path.expanduser('~/thegoodwand/templates'))
+from Services import *
+
+keyword = KeywordService()
+
+def callback(keyword):
+    print(f"callback, recognized {keyword}")
+
+keyword.subscribe(callback)
+
+print("enabling service... say some keywords! (enter to continue)")
+keyword.enable()
+input()
+
+
+print("disabling service... try and say some keywords again nothing should show (enter to continue)")
+keyword.disable()
+input()
