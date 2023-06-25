06600a0934665d2073c5b679a5bda14f8068b822
Jim Horwitz <jhorwitz1011@gmail.com>
2023-06-25 13:04:16 -0400

    conductor bugfixing

---
+++ spells/lumos/main.py
@@ -0,0 +1,24 @@
+import time
+import signal
+import json
+from multiprocessing import Process
+import subprocess
+import logging
+
+import sys
+import os
+sys.path.append(os.path.expanduser('~/thegoodwand/templates'))
+
+#from MQTTObject import MQTTObject
+from Services import *
+from log import log
+
+
+if __name__ == '__main__':
+    mqttobj = MQTTClient()
+    mqttclient = mqttobj.start("lumos")
+
+    lights = LightService()
+    lights.lb_fire(0xaa, 0x42, 0x03)
+    lights.bl_heartbeat(0xaa, 0x42, 0x03)
+    signal.pause()
