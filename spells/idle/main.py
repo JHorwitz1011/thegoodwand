2db8814ec6cb062f2818f3b6209ed8ed63e7f9be
charmley <trevor.charmley@gmail.com>
2023-07-04 20:46:21 -0700

    Fixed termination and wrapped logic in a class

--- spells/idle/main.py
+++ spells/idle/main.py
@@ -1,10 +1,8 @@
 """ Unit tests for Service Files """
-import sys, os, time, signal, math
-from collections import deque
+import sys, os, time, signal
 
 sys.path.append(os.path.expanduser('~/thegoodwand/templates'))
 from Services import MQTTClient
-from Services import ButtonService
 from Services import LightService
 from Services import AudioService
 from Services import IMUService
@@ -16,126 +14,100 @@ LOGGER_NAME = __name__
 logger = log(name = LOGGER_NAME, level = DEBUG_LEVEL)
 
 MQTT_CLIENT_ID = "idle_SPELL"
+TERMINATION_DELAY = 18 # final audio is 16 seconds. lights is 14.5s
 
-idleStep = 1
-IDLE_NUMBER_STEPS = 5
+
+IDLE_NUMBER_STEPS = 4
 IDLE_DELAY = 120 # 2 min
 idleLightRed = 255    #Gold
 idleLightGreen = 215  #Gold
 idleLightBlue = 0     #Gold
 
 
-# Receives "short", "medium", "long"
-def button_callback(press):
-    # User Code Here
-    logger.debug(f"Recieved Press {press}")
-    
+class Idle():
+    def __init__(self) -> None:
+        self.idle_step = 1
+        self.timer = Idle_Timer(IDLE_DELAY, self.timer_callback, name ="idle timer") 
+
+    def start(self):
+        logger.debug("starting idle sequence ")
+        #audio.play_background ("activateIdle.wav") # Dont think we need this 
+        self.timer.start()
         
-def orientation_callback(orientation):
-    logger.debug(f"IMU orientation {orientation}")
-
-
-def display_lights(red,green,blue):
-    lights.lb_fire(red,green,blue)
-
-def idle_timer_callback():
-    global idleStep
-    #global IDLE_NUMBER_STEPS
-
-    fileToPlay = "jmng"+str(idleStep)+".wav"
-    logger.debug(f"idle timer expired at step {idleStep} playing {fileToPlay}")
-    audio.play_background (fileToPlay)
-
-    idleStep += 1
-    if idleStep < IDLE_NUMBER_STEPS:
+    def wait(self):
+        logger.debug("Idle Init done. Waiting for idle event")
+        audio.play_foreground("activatingIdle.wav")
         lights.lb_csv_animation("idleDrumsShort.csv")
-        idle_timer.stop()
-        idle_timer.start()
-        logger.debug(f"restarting idle time for next step")
-    else: 
-        logger.debug(f"Last idle step. Terminating")
-        lights.lb_csv_animation("idleDrumsLong.csv")
-        os._exit(0)
-    
 
+    def timer_callback(self):
+        logger.debug(f"idle timer expired. Idle Step:{self.idle_step}")
+        audio.play_background (f"jmng{self.idle_step}.wav")
+        if self.idle_step < IDLE_NUMBER_STEPS:
+            lights.lb_csv_animation("idleDrumsShort.csv")
+            self.timer.start()
+        else: 
+            lights.lb_csv_animation("idleDrumsLong.csv")
+            delayed_terminnation(TERMINATION_DELAY)
+
+        self.idle_step += 1
+
+
+def display_fire(red,green,blue):
+    lights.lb_fire(red,green,blue)
 
 def imu_on_wake_callback(wake_status:'bool'):
     if wake_status == True: 
-        logger.debug(f"Wand is active, terminate idle spell")
-        time.sleep(10)
-        lights.lb_block(0,0,0)
-        os._exit(0)
+        logger.debug(f"Wand is active")
+        idle_spell.timer.stop()
+        delayed_terminnation(0)
 
     elif wake_status == False: 
-        logger.debug(f"Wand is inactive, activate")
-        audio.play_background ("activateIdle.wav")
-        display_lights (idleLightRed, idleLightGreen,idleLightBlue) 
-        idle_timer.start()
-
-    else:    
-        logger.warning(f"Unknown active state {wake_status}")
-
-# Initialize button. return button object
-def init_button(mqtt_client, callback):
-    button = ButtonService(mqtt_client)
-    button.subscribe(button_callback)
-    return button 
-
-def init_lights(mqtt_client, path): 
-    return LightService(mqtt_client = mqtt_client, path = path)
-
-def init_audio(mqtt_client, path):
-    return AudioService(mqtt_client = mqtt_client, path = path)
+        logger.debug(f"Wand is inactive")
+        idle_spell.start()
 
 def init_imu(mqtt_client, on_wake_cb):
     imu = IMUService(mqtt_client)
     imu.subscribe_on_wake(on_wake_cb)
     return imu
 
-# Cleanup 
-def terminate_spell(sig, frame): 
+def delayed_terminnation(delay):
+    logger.debug(f"Terminating idle spell: Delay {delay}")
+    if delay: time.sleep(delay) # Delay to allow final animation to finish
     logger.debug("Terminating Idle Spell")
-    lights.lb_block(0,0,0)
-    time.sleep(.1)
+    lights.lb_clear()
+    audio.stop()
+    time.sleep(.25)
     os._exit(0)
-    
+
+def sig_callback(sig, frame):
+    idle_spell.timer.stop()
+    delayed_terminnation(0)
+
+def get_path():
+    param_1 = None
+    if len(sys.argv) < 2:
+        logger.debug("No arguments provided.")
+    else:
+        param_1 = sys.argv[1]
+    return param_1 if param_1 else os.getcwd()
+
 
 if __name__ == '__main__':
     # Connect to MQTT and get client instance 
     logger.debug("Starting idle spell")
     mqtt_object = MQTTClient()
     mqtt_client = mqtt_object.start(MQTT_CLIENT_ID)
+    spellPath = get_path()
 
-    # Get spell activation arguments. #1 is path, #2 is other args
-    param_1 = ""
-    param_2 = ""
-    if len(sys.argv) > 1 :
-        param_1= sys.argv[1] 
-    
-    if len(sys.argv) > 2 :
-        param_2= sys.argv[2] 
+    audio   = AudioService(mqtt_client, spellPath)
+    lights  = LightService(mqtt_client, spellPath)
+    imu     = init_imu(mqtt_client, imu_on_wake_callback)
 
-	    
-    # if started by conductor, param1 is the path,
-	# otherwise use cwd
-    
-    if param_1 !="":
-        spellPath = param_1
-    else:
-        spellPath =os.getcwd() 
-
-    audio   = init_audio(mqtt_client, spellPath)
-    button  = init_button(mqtt_client, button_callback)
-    lights  = init_lights(mqtt_client, spellPath)
-    imu     = init_imu(mqtt_client, on_wake_cb = imu_on_wake_callback)
-
-    idle_timer = Idle_Timer(IDLE_DELAY, idle_timer_callback, name ="idle timer") 
-       
-    audio.play_foreground("activatingIdle.wav")
-    
-    signal.signal(signal.SIGINT, terminate_spell)
-    signal.signal(signal.SIGTERM, terminate_spell)
-    logger.debug("Idle Init done. Waiting for events")
-    idle_timer.start()
+    idle_spell = Idle()
+    # possible bug at the wait state if the wand is alreay in idle. 
+    # This will be fixed when the conductor handles the idle / launching of this spell
+    idle_spell.wait() 
+    signal.signal(signal.SIGINT, sig_callback)
+    signal.signal(signal.SIGTERM, sig_callback)
     signal.pause()
 
