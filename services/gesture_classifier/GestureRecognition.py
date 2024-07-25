865113433a3cf3911175a7c869083dba81c93229
Jim Horwitz <jhorwitz1011@gmail.com>
2024-07-25 18:17:40 -0400

    update gesture recognition service

--- services/gesture_classifier/GestureRecognition.py
+++ services/gesture_classifier/GestureRecognition.py
@@ -136,22 +136,22 @@ class GestureClassifier():
                 print(self.values)
 
                 classification = result['classification']
-                # if not self._isAnomaly(classification['unknown']):
-                if self._isGestureDetected(classification['jab']):
-                    GESTURE_TEMP_PKT["data"] = {"gesture":"jab"}
-                    self.mqtt_client.publish(GESTURE_TOPIC, json.dumps(GESTURE_TEMP_PKT))
-                    time.sleep(COOLDOWN)
-                    # self.values = BUFFER_SIZE*[0]
-                elif self._isGestureDetected(classification['channel']):
-                    GESTURE_TEMP_PKT["data"] = {"gesture":"channel"}
-                    self.mqtt_client.publish(GESTURE_TOPIC, json.dumps(GESTURE_TEMP_PKT))
-                    time.sleep(COOLDOWN)
-                    # self.values = BUFFER_SIZE*[0]
-                elif self._isGestureDetected(classification['flick']):
-                    GESTURE_TEMP_PKT["data"] = {"gesture":"flick"}
-                    self.mqtt_client.publish(GESTURE_TOPIC, json.dumps(GESTURE_TEMP_PKT))
-                    time.sleep(COOLDOWN)
-                    # self.values = BUFFER_SIZE*[0]
+                if not self._isAnomaly(classification['unknown']):
+                    if self._isGestureDetected(classification['jab']):
+                        GESTURE_TEMP_PKT["data"] = {"gesture":"jab"}
+                        self.mqtt_client.publish(GESTURE_TOPIC, json.dumps(GESTURE_TEMP_PKT))
+                        time.sleep(COOLDOWN)
+                        # self.values = BUFFER_SIZE*[0]
+                    elif self._isGestureDetected(classification['channel']):
+                        GESTURE_TEMP_PKT["data"] = {"gesture":"channel"}
+                        self.mqtt_client.publish(GESTURE_TOPIC, json.dumps(GESTURE_TEMP_PKT))
+                        time.sleep(COOLDOWN)
+                        # self.values = BUFFER_SIZE*[0]
+                    elif self._isGestureDetected(classification['flick']):
+                        GESTURE_TEMP_PKT["data"] = {"gesture":"flick"}
+                        self.mqtt_client.publish(GESTURE_TOPIC, json.dumps(GESTURE_TEMP_PKT))
+                        time.sleep(COOLDOWN)
+                        # self.values = BUFFER_SIZE*[0]
                 time.sleep(1/MODEL_RATE)
         finally:
             if (self.runner):
