755895a16de5ce1179c86323428d41d850b8a49f
Jim Horwitz <jhorwitz1011@gmail.com>
2024-08-13 00:02:28 -0400

    mirror main

--- spells/mirror/main.py
+++ spells/mirror/main.py
@@ -60,6 +60,7 @@ def gesture_callback(param):
     print("GESTURE RECOGNIZED, ", param)
     global gesture_completed
     if not gesture_completed:
+        print("gesture incomplete")
         if param == target_gesture:
             gesture_completed = True
             success()
