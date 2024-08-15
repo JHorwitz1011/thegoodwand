8ced32d28bdc8a09469b7016a151b41d61abe768
Jim Horwitz <jhorwitz1011@gmail.com>
2024-08-15 16:17:38 -0400

    numeros

--- spells/numeros/main.py
+++ spells/numeros/main.py
@@ -7,7 +7,7 @@ speaker = pyttsx3.init()
 speaker.setProperty('rate', 150)  # Speed of speech (words per minute)
 speaker.setProperty('volume', 1.0)  # Volume (0.0 to 1.0)
 # Text to be converted to speech
-text = "Hello. Let's count!"
+text = "Cheetos!"
 # Convert text to speech and play it
 speaker.say(text)
 # Wait for the speech to finish
