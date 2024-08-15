32c5c7eee41f10ca94f7fc015e3d777ecc432e23
Jim Horwitz <jhorwitz1011@gmail.com>
2024-08-15 16:20:12 -0400

    numeros

--- spells/numeros/main.py
+++ spells/numeros/main.py
@@ -6,9 +6,12 @@ speaker = pyttsx3.init()
 # Set properties (optional)
 speaker.setProperty('rate', 150)  # Speed of speech (words per minute)
 speaker.setProperty('volume', 1.0)  # Volume (0.0 to 1.0)
-# Text to be converted to speech
-text = "Cheetos!"
-# Convert text to speech and play it
-speaker.say(text)
-# Wait for the speech to finish
-speaker.runAndWait()
+voices = speaker.getProperty('voices')
+for voice in voices:
+    speaker.setProperty(voice)
+    # Text to be converted to speech
+    text = "Cheetos!"
+    # Convert text to speech and play it
+    speaker.say(text)
+    # Wait for the speech to finish
+    speaker.runAndWait()
